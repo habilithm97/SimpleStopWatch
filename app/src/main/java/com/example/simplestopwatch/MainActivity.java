843550c3ab53aa398a6b569bbc58a3edad12af90
habilithm97 <habinkim97@gmail.com>
2022-06-20 17:35:13 +0900

    스톱워치 UI

--- app/src/main/java/com/example/simplestopwatch/MainActivity.java
+++ app/src/main/java/com/example/simplestopwatch/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.simplestopwatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +12,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 }

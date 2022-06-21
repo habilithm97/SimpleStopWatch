86f303074dc503266b5d18a18423712a65b6bf8e
habilithm97 <habinkim97@gmail.com>
2022-06-21 23:38:54 +0900

    시작 버튼을 눌러 스톱워치 실행

--- app/src/main/java/com/example/simplestopwatch/MainActivity.java
+++ app/src/main/java/com/example/simplestopwatch/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.simplestopwatch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Thread timeThread = null;
+    private Boolean isRunning = true;
+
+    TextView timeTv;
     Button startBtn;
     LinearLayout layout, layout2;
 
@@ -18,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView timeTv = (TextView)findViewById(R.id.timeTv);
+        timeTv = (TextView)findViewById(R.id.timeTv);
 
         startBtn = (Button)findViewById(R.id.startBtn);
         startBtn.setOnClickListener(new View.OnClickListener() {
@@ -62,6 +71,10 @@ public class MainActivity extends AppCompatActivity {
     public void startTimer() {
         startBtn.setVisibility(View.GONE);
         layout.setVisibility(View.VISIBLE);
+
+        // 스레드를 생성해서 실행함
+        timeThread = new Thread(new TimeThread());
+        timeThread.start();
     }
 
     public void stopTimer() {
@@ -77,5 +90,50 @@ public class MainActivity extends AppCompatActivity {
     public void resetTimer() {
         layout2.setVisibility(View.GONE);
         startBtn.setVisibility(View.VISIBLE);
+    }
+
+    // 핸들러를 이용해서 UI를 변경할 수 있음
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            int mSec = msg.arg1 % 100;
+            int sec = (msg.arg1 / 100) % 60;
+            int min = (msg.arg1 / 100) / 60;
+            int hour = (msg.arg1 / 100) / 360;
+
+           String result = String.format("%02d:%02d:%02d.%02d", hour, min, sec, mSec);
+           timeTv.setText(result);
+        }
+    };
+
+    public class TimeThread implements Runnable {
+        @Override
+        public void run() {
+            int i = 0;
+
+            while(true) {
+                while(isRunning) {
+                    Message msg = new Message();
+                    msg.arg1 = i++;
+                    handler.sendMessage(msg);
+
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        // runOnUiThread() : Runnable 객체에 구현된 코드를 반드시 메인 스레드에서 실행해야할 때 사용하는 메서드
+                        runOnUiThread(new Runnable() { // Runnable 객체를 메인 스레드에서 실행되도록 만드는 메서드로서, 현재 스레드가
+                            // 메인 스레드인지 여부를 검사하여 메인 스레드가 아니라면 post()를 실행하고, 메인 스레드라면 Runnable의 run()을 실행함
+                            @Override
+                            public void run() {
+                                timeTv.setText("");
+                                timeTv.setText("00:00:00.00");
+                            }
+                        });
+                        return;
+                    }
+                }
+            }
+        }
     }
 }
